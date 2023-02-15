e0d95c6df93609bbeaee11932ca6e50fcb810752
JinYoung Park <deneb.jin@gmail.com>
2023-02-15 18:11:56 +0900

    저장폴더 수정함

--- xlwings_unmerge.py
+++ xlwings_unmerge.py
@@ -1,10 +1,12 @@
+print('로딩중..........')
 import xlwings as xw
 from tkinter import filedialog
 from tkinter import messagebox
 from tqdm import tqdm 
+import os
 
 list_file = []                                          #파일 목록 담을 리스트 생성
-
+print('파일을 선택창을 띄웁니다.')
 def loadingfile():
     files = filedialog.askopenfilenames(initialdir="/",title = "파일을 선택 해 주세요",filetypes = (("*.xlsx","*xlsx"),("*.xls","*xls"),("*.csv","*csv")))
     if files =='':
@@ -29,8 +31,9 @@ else:
                 workrange = rngC.merge_area
                 rngC.unmerge()
                 workrange.value=rngC.value
-    wb.save('./unmerged.xlsx')
-    messagebox.showinfo("완료", "unmerged.xlsx로 저장했습니다.")
+    dirpath=os.path.dirname(loadedfile)
+    wb.save(dirpath+'/Unmerged.xlsx')
+    messagebox.showinfo("완료", "원본폴더에 Unmerged.xlsx로 저장했습니다.")
     wb.close()
 quit()
 
