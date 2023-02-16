fc0f9157a3f2d1726c14a2a809927af7bc926688
JinYoung Park <deneb.jin@gmail.com>
2023-02-16 11:39:39 +0900

    함수위치변경함

--- xlwings_unmerge.py
+++ xlwings_unmerge.py
@@ -5,16 +5,22 @@ from tkinter import messagebox
 from tqdm import tqdm 
 import os
 
-list_file = []                                          #파일 목록 담을 리스트 생성
-print('파일을 선택창을 띄웁니다.')
-def loadingfile():
-    files = filedialog.askopenfilenames(initialdir="/",title = "파일을 선택 해 주세요",filetypes = (("*.xlsx","*xlsx"),("*.xls","*xls"),("*.csv","*csv")))
+def loadfile():
+    """파일을 불러오는 함수
+    
+    Returns:
+        파일경로 및 파일명
+    """
+    files = filedialog.askopenfilenames(initialdir="/",title = "파일을 선택 해 주세요",filetypes = (("*.xlsx","*xlsx"),("*.xls","*xls"),("*.xlsm","*xlsm")))
     if files =='':
-        return ''
+        return '' 
     else:
         return files[0]
         
-loadedfile = loadingfile()
+
+list_file = []                                          #파일 목록 담을 리스트 생성
+print('파일을 선택창을 띄웁니다.')
+loadedfile = loadfile()
 if loadedfile == '':
     messagebox.showwarning("경고", "파일을 추가하고 다시 실행하세요")    #파일 선택 안했을 때 메세지 출력
 
@@ -35,7 +41,6 @@ else:
     wb.save(dirpath+'/Unmerged.xlsx')
     messagebox.showinfo("완료", "원본폴더에 Unmerged.xlsx로 저장했습니다.")
     wb.close()
-    
 quit()
 
 
